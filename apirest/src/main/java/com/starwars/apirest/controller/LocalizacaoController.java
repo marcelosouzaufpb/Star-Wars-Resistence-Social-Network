817b143a9c918ac954172fe7c87a78ac568591d0
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-03 15:09:09 -0300

    #controller-localizacao - add metodo litar localizacao, retornando uma lista de localizacoes

--- apirest/src/main/java/com/starwars/apirest/controller/LocalizacaoController.java
+++ apirest/src/main/java/com/starwars/apirest/controller/LocalizacaoController.java
@@ -1,5 +1,26 @@
 package com.starwars.apirest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.starwars.apirest.models.Localizacao;
+import com.starwars.apirest.repository.LocalizacaoRepository;
+
+@RestController
+@RequestMapping(value = "/api")
 public class LocalizacaoController {
+
+	@Autowired
+	LocalizacaoRepository localizacaoRepository;
+	
+
+	@GetMapping("/localizacao")
+	public List<Localizacao> listaProdutos() {
+		return localizacaoRepository.findAll();
+	}
 
 }
