fb980410fa157da7beb0a459179ccf0d3f5b3774
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-03 15:14:50 -0300

    #controller-localizacao - add metodo get localizacao por id, que retorna uma localizacao pelo id

--- apirest/src/main/java/com/starwars/apirest/controller/LocalizacaoController.java
+++ apirest/src/main/java/com/starwars/apirest/controller/LocalizacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class LocalizacaoController {
 	@GetMapping("/localizacao")
 	public List<Localizacao> listaProdutos() {
 		return localizacaoRepository.findAll();
+	}
+	
+	@GetMapping("/localizacao/{id}")
+	public Localizacao getLocalizacaoById(@PathVariable(value = "id") Integer id) {
+		return localizacaoRepository.findById(id);
 	}
 
 }
--- apirest/src/main/java/com/starwars/apirest/repository/LocalizacaoRepository.java
+++ apirest/src/main/java/com/starwars/apirest/repository/LocalizacaoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starwars.apirest.models.Localizacao;
 
-public interface LocalizacaoRepository extends JpaRepository<Localizacao, Integer> {
+public interface LocalizacaoRepository extends JpaRepository<Localizacao, Long> {
 
+	public Localizacao findById(Integer id);
 }
