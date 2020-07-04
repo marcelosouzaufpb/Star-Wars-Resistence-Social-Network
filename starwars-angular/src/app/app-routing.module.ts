81b7b50e2ef27f93b903b6882171cafec650c7a4
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-04 19:13:56 -0300

    #telas - finalizando tela de registro de rebelde

--- starwars-angular/src/app/app-routing.module.ts
+++ starwars-angular/src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { GameComponent } from './views/game/game.component';
+import { LocalizacaoComponent } from './views/localizacao/localizacao.component';
+import { ReporteComponent } from './views/reporte/reporte.component';
+import { RegistroComponent } from './views/registro/registro.component';
+import { NegocioComponent } from './views/negocio/negocio.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'game', component: GameComponent }
+  { path: 'game', component: GameComponent },
+  { path: 'game/negocio', component: NegocioComponent },
+  { path: 'game/registro', component: RegistroComponent },
+  { path: 'game/reporte', component: ReporteComponent },
+  { path: 'game/localizacao', component: LocalizacaoComponent }
 ];
 
 @NgModule({
