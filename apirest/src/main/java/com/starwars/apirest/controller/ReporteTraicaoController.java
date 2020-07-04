5cb9203e9090d836d43708f5dc6cde8920bbc8a4
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-03 22:36:16 -0300

    #swagger - add anotacoes do swagger

--- apirest/src/main/java/com/starwars/apirest/controller/ReporteTraicaoController.java
+++ apirest/src/main/java/com/starwars/apirest/controller/ReporteTraicaoController.java
@@ -3,6 +3,7 @@ package com.starwars.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,34 +16,44 @@ import org.springframework.web.bind.annotation.RestController;
 import com.starwars.apirest.models.ReporteTraicao;
 import com.starwars.apirest.service.ReporteTraicaoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST Reporte Traicao")
 public class ReporteTraicaoController {
 
 	@Autowired
 	private ReporteTraicaoService service;
 
 	@GetMapping("/reporteTraicao")
+	@ApiOperation(value = "Retorna uma lista de Reporte Traicao")
 	public List<ReporteTraicao> listaReporteTraicao() {
 		return this.service.findAll();
 	}
 
 	@GetMapping("/reporteTraicao/{id}")
+	@ApiOperation(value = "Retorna um Reporte Traicao")
 	public ReporteTraicao getReporteTraicaoById(@PathVariable(value = "id") Integer id) {
 		return this.service.findById(id);
 	}
 
 	@PostMapping("/reporteTraicao")
+	@ApiOperation(value = "Salva um Reporte Traicao no DB")
 	public ReporteTraicao salvarReporteTraicao(@RequestBody ReporteTraicao reporteTraicao) {
 		return this.service.save(reporteTraicao);
 	}
 
 	@DeleteMapping("/reporteTraicao")
+	@ApiOperation(value = "Deleta um Reporte Traicao no DB")
 	public void deletaReporteTraicao(@RequestBody ReporteTraicao reporteTraicao) {
 		this.service.delete(reporteTraicao);
 	}
 
 	@PutMapping("/reporteTraicao")
+	@ApiOperation(value = "Atualiza um Reporte Traicao no DB")
 	public ReporteTraicao atualizaReporteTraicao(@RequestBody ReporteTraicao reporteTraicao) {
 		return this.service.update(reporteTraicao);
 	}
