ac70b2cb1cd110455ae233c1ad83296c897ed074
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-04 16:08:20 -0300

    configurando rota para game

--- starwars-angular/src/app/app-routing.module.ts
+++ starwars-angular/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { GameComponent } from './views/game/game.component';
 
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent
-  }
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
