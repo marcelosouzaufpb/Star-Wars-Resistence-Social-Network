e7db50ddf6f0da8799d5fbf8ed0c8519557faf83
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-04 21:18:05 -0300

    #telas - concluindo basico das telas

--- starwars-angular/src/app/views/negocio/negocio.component.ts
+++ starwars-angular/src/app/views/negocio/negocio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgOption } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-negocio',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NegocioComponent implements OnInit {
 
+  countries: NgOption[] = [
+    {id: 10, name: 'POLAND'},
+    {id: 20, name: 'UK'},
+    {id: 30, name: 'GERMANY'},
+    {id: 40, name: 'NORWAY'},
+    {id: 50, name: 'FINLAND'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChange = ($event: any): void => {
+    console.log($event);
+    console.log(`SELECTION CHANGED INTO ${$event.name || ''}`);
   }
 
 }
