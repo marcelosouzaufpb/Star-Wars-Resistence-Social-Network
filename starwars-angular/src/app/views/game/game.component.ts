19ffd0977ca08e4061e6bc97d808909ea8b39a29
Marcelo Souza Santos <marcelo.santos@amcom.com.br>
2020-07-05 17:05:20 -0300

    #conexao - fechando relatorio

--- starwars-angular/src/app/views/game/game.component.ts
+++ starwars-angular/src/app/views/game/game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RebeldeService } from 'src/app/shared/service/rebelde.service';
+import { Rebelde } from 'src/app/shared/model/rebelde.model';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  rebeldes: Rebelde[];
+  rebeldesNTraidores: Rebelde[];
+  
+  constructor(public rebeldeService: RebeldeService) { 
+    this.rebeldes = [];
+    this.rebeldesNTraidores = [];
+  }
 
   ngOnInit(): void {
+    this.getRebeldes();
+ }
+
+  getRebeldes(): void {
+    this.rebeldeService.get().subscribe((result: any) => {
+      this.rebeldes = result;
+      this. getNTraidores();
+    });
+  }
+
+  getNTraidores(): void {
+    for (let rebelde of this.rebeldes) {
+      if (rebelde.eTraidor !== true) {
+        this.rebeldesNTraidores.push(rebelde);
+      }
+    }
   }
 
 }
